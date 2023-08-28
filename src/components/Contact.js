c9324cf8b562b6978c2d879230957e0617898f71
tinpham5614 <tinpham5614@github.com>
2023-08-28 14:38:08 -0700

    upload files

---
+++ src/components/Contact.js
@@ -0,0 +1,94 @@
+import { useForm } from "react-hook-form";
+import {
+  BookOpenIcon,
+  ChatBubbleBottomCenterIcon,
+} from "@heroicons/react/24/solid";
+import emailjs from "@emailjs/browser";
+import env from "react-dotenv";
+
+export default function App() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    alert(`🔔 Nice to meet you ${data.name}! Happy coding 😊!`);
+    emailjs
+      .sendForm(
+        env.MY_SERVICE_ID,
+        env.MY_TEMPLATE_ID,
+        document.querySelector("form"),
+        env.MY_PUBLIC_KEY
+      )
+      .then((result) => {
+        console.log(result.text);
+      })
+      .catch((error) => {
+        console.log(error.text);
+      });
+  };
+
+  return (
+    <section id="contact" className="relative text-center">
+      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+        <BookOpenIcon className="mx-auto inline-block w-10 mt-4 text-gray-800" />
+        <h1 className="sm:text-4xl font-medium title-font text-gray-800 mt-4 text-center">
+          Contact
+        </h1>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="mx-auto mt-8 w-1/2 text-left"
+        >
+          <div className="flex flex-col sm:flex-row -mx-3 mb-6">
+            <div className="flex-1 mx-3">
+              <label className="block mb-2 text-gray-800" htmlFor="name">
+                Name
+              </label>
+              <input
+                className="w-full px-4 py-2 text-gray-800 bg-gray-100 rounded border-2 border-gray-800"
+                {...register("name")}
+              />
+            </div>
+            <div className="flex-1 mx-3">
+              <label className="block mb-2 text-gray-800" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="w-full px-4 py-2 text-gray-800 bg-gray-100 rounded border-2 border-gray-800"
+                {...register("email")}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col sm:flex-row -mx-3 mb-6 text-center">
+            <div className="flex-1 mx-3">
+              <label className="block mb-2 text-gray-800" htmlFor="message">
+                Message
+              </label>
+              <textarea
+                className="w-full px-4 py-2 text-gray-800 bg-gray-100 rounded border-2 border-gray-800"
+                {...register("message")}
+              />
+            </div>
+          </div>
+          <button className="block w-full px-4 py-3 mt-8 text-lg font-medium text-white bg-gray-800 rounded-lg">
+            Send Message
+          </button>
+        </form>
+        <footer className="text-gray-800 body-font">
+          <div className="container px-5 mt-8 mx-auto flex items-center sm:flex-row flex-col">
+            <p className="text-sm text-gray-800 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4 right-0">
+              © 2023 Tin Pham
+            </p>
+            <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+              <a
+                href="https://github.com/tinpham5614"
+                target="_blank"
+                rel="noreferrer"
+                className="text-gray-800"
+              >
+                <ChatBubbleBottomCenterIcon className="w-5 h-5" />
+              </a>
+            </span>
+          </div>
+        </footer>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,30 @@
+import project1_image from "./images/project1.png";
+import project2_image from "./images/project2.png";
+import project3_image from "./images/project3.png";
+
+export const projects = [
+  {
+    title: "Health Finder Search App",
+    subtitle: "ReactJS",
+    description:
+      "Developed a responsive and mobile-friendly health information search engine using React and an API from the U.S. Department of Health and Human Services. Provided health recommendations for various age groups and helped individuals take preventive measures to maintain their health",
+    image: project1_image,
+    link: "https://tinpham5614.github.io/HealthFinder/",
+  },
+  {
+    title: "Belinda's Closet",
+    subtitle: "NextJS and Android",
+    description:
+      "Developed Belinda's Closet application to cater to students' graduation day clothing needs, later expanding to include professional attire for interviews and work. Implemented a user-friendly platform accessible on desktop and mobile devices, empowering staff to manage inventory and enabling seamless clothing searches for students",
+    image: project2_image,
+    link: "/",
+  },
+  {
+    title: "North Seattle College Events",
+    subtitle: "NextJS and Android",
+    description:
+      "Developed a cohesive and user-friendly application that enables school staff and admin to organize events in a visible manner, providing students with a more manageable and interactive platform compared to the overwhelming Canvas notifications. Implemented distinct sections within the app, allowing students to personalize their experience by organizing and accessing events based on their interests, fostering a more engaging and purpose-driven user interaction.",
+    image: project3_image,
+    link: "/",
+  },
+];
